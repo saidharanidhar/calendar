8af27ba89b549fe46aca135d9557b7f5c438d1b7
Dharanidhar Reddy <dharanidhar01.04@gmail.com>
2024-05-13 01:33:59 +0530

    feat: viewDropDown Button Component

--- packages/core/src/Buttons.svelte
+++ packages/core/src/Buttons.svelte
@@ -1,14 +1,35 @@
 <script>
-    import {getContext} from 'svelte';
-    import {createDate, cloneDate, subtractDay, addDuration, setContent, subtractDuration, setMidnight} from './lib.js';
+    import { getContext } from "svelte";
+    import {
+        createDate,
+        cloneDate,
+        subtractDay,
+        addDuration,
+        setContent,
+        subtractDuration,
+        setMidnight,
+    } from "./lib.js";
 
     export let buttons;
 
-    let {_currentRange, _viewTitle, buttonText, customButtons, date, duration, hiddenDays, theme, view} = getContext('state');
+    let {
+        _currentRange,
+        _viewTitle,
+        buttonText,
+        customButtons,
+        date,
+        duration,
+        hiddenDays,
+        theme,
+        view,
+        viewDropDownOptions,
+    } = getContext("state");
 
-    let today = setMidnight(createDate()), isToday;
+    let today = setMidnight(createDate()),
+        isToday;
 
-    $: isToday = today >= $_currentRange.start && today < $_currentRange.end || null;
+    $: isToday =
+        (today >= $_currentRange.start && today < $_currentRange.end) || null;
 
     function prev() {
         let d = subtractDuration($date, $duration);
@@ -26,38 +47,47 @@
 </script>
 
 {#each buttons as button}
-    {#if button == 'title'}
+    {#if button == "title"}
         <!-- svelte-ignore a11y-missing-content -->
-        <h2 class="{$theme.title}" use:setContent={$_viewTitle}></h2>
-    {:else if button == 'prev'}
+        <h2 class={$theme.title} use:setContent={$_viewTitle}></h2>
+    {:else if button == "prev"}
         <button
             class="{$theme.button} ec-{button}"
             aria-label={$buttonText.prev}
             title={$buttonText.prev}
-            on:click={prev}
-        ><i class="{$theme.icon} ec-{button}"></i></button>
-    {:else if button == 'next'}
+            on:click={prev}><i class="{$theme.icon} ec-{button}"></i></button
+        >
+    {:else if button == "next"}
         <button
             class="{$theme.button} ec-{button}"
             aria-label={$buttonText.next}
             title={$buttonText.next}
-            on:click={next}
-        ><i class="{$theme.icon} ec-{button}"></i></button>
-    {:else if button == 'today'}
+            on:click={next}><i class="{$theme.icon} ec-{button}"></i></button
+        >
+    {:else if button == "today"}
         <button
             class="{$theme.button} ec-{button}"
-            on:click={() => $date = cloneDate(today)}
-            disabled={isToday}
-        >{$buttonText[button]}</button>
+            on:click={() => ($date = cloneDate(today))}
+            disabled={isToday}>{$buttonText[button]}</button
+        >
+    {:else if button == "viewDropDown"}
+        <select class="{$theme.button} ec-{button}" bind:value={$view}>
+            {#each $viewDropDownOptions as key (key)}
+                <option value={key}>{$buttonText[key]}</option>
+            {/each}
+        </select>
     {:else if $customButtons[button]}
         <button
             class="{$theme.button} ec-{button}"
             on:click={$customButtons[button].click}
-        >{$customButtons[button].text}</button>
-    {:else if button != ''}
+            >{$customButtons[button].text}</button
+        >
+    {:else if button != ""}
         <button
-            class="{$theme.button}{$view === button ? ' ' + $theme.active : ''} ec-{button}"
-            on:click={() => $view = button}
-        >{$buttonText[button]}</button>
+            class="{$theme.button}{$view === button
+                ? ' ' + $theme.active
+                : ''} ec-{button}"
+            on:click={() => ($view = button)}>{$buttonText[button]}</button
+        >
     {/if}
 {/each}
