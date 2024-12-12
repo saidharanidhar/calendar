7caf35fef0b2b8cd1bfa72ba2fa66e8043ce5d98
Dharanidhar Reddy <dharanidhar01.04@gmail.com>
2024-12-13 00:06:48 +0530

    feat(resource-timeline): Customised the library and updated the row-height from 34px to 64px

--- packages/resource-timeline/src/Expander.svelte
+++ packages/resource-timeline/src/Expander.svelte
@@ -34,7 +34,7 @@
 <span class="{$theme.expander}">
     {#if payload.children.length}
         <button class="{$theme.button}" on:click={handleClick}>
-            {#if payload.expanded}&minus;{:else}&plus;{/if}
+            {#if payload.expanded}&#9650;{:else}&#9660;{/if}
         </button>
     {/if}
 </span>
