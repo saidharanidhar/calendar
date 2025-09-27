ea2324422eaa039e8bb5649f1468bdc7ca4d03cc
Dharanidhar Reddy <dharanidhar01.04@gmail.com>
2025-09-27 22:09:16 +0530

    chore: Fixes for Staffing View

--- packages/core/src/plugins/resource-timeline/Sidebar.svelte
+++ packages/core/src/plugins/resource-timeline/Sidebar.svelte
@@ -17,7 +17,7 @@
     <div class="{$theme.sidebarTitle}" style="flex-basis: {$_headerHeight}px"></div>
     <div class="{$theme.content}" bind:this={$_sidebarEl} {onwheel}>
         {#each $_viewResources as resource}
-            <div class="{$theme.resource}" style="flex-basis: {64 || max($_resHs.get(resource) ?? 0, 64)}px" role="rowheader">
+            <div class="{$theme.resource}" style="flex-basis: {max($_resHs.get(resource) ?? 0, 64)}px" role="rowheader">
                 {#if $_nestedResources}
                     <Expander {resource} />
                 {/if}
